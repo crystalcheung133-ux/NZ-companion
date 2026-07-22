50018e2b736688ba3d50765b90347756e49035e5
crystalcheung133-ux <crystalcheung133@gmail.com>
2026-07-22 12:45:13 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('./theme-config.js', './asset-config.js', './locale-config.js', './formatter.js', './navigation-config.js', './storage-config.js', './trip-config.js');
-const CACHE_NAME = `travel-engine-${TRIP_CONFIG.storageNamespace}-${TRIP_CONFIG.version}-rc14a-trip-identity-extraction`;
+const CACHE_NAME = `travel-engine-${TRIP_CONFIG.storageNamespace}-${TRIP_CONFIG.version}-rc14b-production-consistency-home-ux`;
 const CRITICAL_EXTENSIONS = /\.(?:css|js)$/i;
 const ASSETS = [
   './',
